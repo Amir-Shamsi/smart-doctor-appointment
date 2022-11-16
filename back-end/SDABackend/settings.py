cff85465703ce89a05ed1a673fb4bf6d3f0b7570
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-11-16 16:49:53 +0330

    initialize the `rest framework` and `djoser`

--- back-end/SDABackend/settings.py
+++ back-end/SDABackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'djoser',
 
     'appAuth'
 ]
@@ -86,6 +89,27 @@ DATABASES = {
     }
 }
 
+# Rest Framework & djoser
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+}
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'appAuth.serializers.UserCreateSerializer',
+
+    }
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
