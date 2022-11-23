cb030d983ee793c0351ba2f59e2e117247414f8a
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-11-23 08:27:24 +0330

    Update urls.py

--- back-end/SDABackend/urls.py
+++ back-end/SDABackend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('appAuth.urls')),
 ]
