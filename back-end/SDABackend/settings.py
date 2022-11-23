7dcd1ba319da204e0b8ed39aac225f9a1b46d913
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-11-23 08:26:05 +0330

    user agent were added, templates & email configured

--- back-end/SDABackend/settings.py
+++ back-end/SDABackend/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'djoser',
+    'django_user_agents',
 
     'appAuth'
 ]
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'SDABackend.urls'
@@ -60,7 +64,7 @@ ROOT_URLCONF = 'SDABackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -153,3 +157,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "appAuth.CustomUser"
+
+# Email Configuration
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER=f'{config("EMAIL_HOST")}@gmail.com'
+EMAIL_HOST_PASSWORD=config('EMAIL_PASSWORD')
+EMAIL_USE_TLS=True
