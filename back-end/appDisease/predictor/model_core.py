30b27b4f46d160426f2d99f0e2ddc530c04890ed
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-12-24 21:50:56 +0330

    create initialize function for the `D.Pred` class

--- back-end/appDisease/predictor/model_core.py
+++ back-end/appDisease/predictor/model_core.py
@@ -10,4 +10,21 @@ from sklearn.naive_bayes import MultinomialNB
 class DiseasePrediction:
     def __init__(self):
         self.clf = None
+        self.__load__conf__()
+        self.__init__vars__()
+        self.__load__dset__()
+
+
+    def __load__conf__(self):
+        with open(f'{str(settings.BASE_DIR)}/appDisease/predictor/config.yaml', 'r') as f:
+            self.config = yaml.safe_load(f)
+
+    def __init__vars__(self):
+        self.verbose = self.config['verbose']
+        self.model_name = self.config['model_type']
+        self.model_save_path = self.config['model_save_path']
+
+    def __load__dset__(self):
+        self.train_features, self.train_labels, self.train_df = self._load_train_dataset()
+        self.test_features, self.test_labels, self.test_df = self._load_test_dataset()
 
