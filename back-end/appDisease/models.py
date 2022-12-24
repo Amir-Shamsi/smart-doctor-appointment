5e71c3a7d038a52e4aa5cd558042793206f292f1
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-12-24 12:24:21 +0330

    general question model added

--- back-end/appDisease/models.py
+++ back-end/appDisease/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class QuestionIntro(models.Model):
+    question = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question
