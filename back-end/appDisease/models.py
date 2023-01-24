e3e4eb9f6d88c90ae1420b3c511274d901be9c1f
Mohammad Keshavarzi <keshavarzi.m380@gmail.com>
2023-01-24 20:35:44 +0330

    model "Recipe" created

--- back-end/appDisease/models.py
+++ back-end/appDisease/models.py
@@ -16,3 +16,10 @@ class Medicine(models.Model):
     def __str__(self) -> str:
         return self.name
     
+
+class Recipe(models.Model):
+    doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="recipe")
+    patient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="patient-recipe")
+    created_at = models.DateTimeField(auto_now=True)
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE, related_name="needed-drugs")
+    description = models.CharField(max_length=2000)
