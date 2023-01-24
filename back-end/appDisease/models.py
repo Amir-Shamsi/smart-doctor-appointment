068a6bb67615877495abdbf654c858860b04e001
Mohammad Keshavarzi <keshavarzi.m380@gmail.com>
2023-01-24 20:35:15 +0330

    model "Medicine" created

--- back-end/appDisease/models.py
+++ back-end/appDisease/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 class QuestionIntro(models.Model):
     question = models.CharField(max_length=255)
 
     def __str__(self):
         return self.question
+
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=256, blank=False)
+    amount = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.name
+    
