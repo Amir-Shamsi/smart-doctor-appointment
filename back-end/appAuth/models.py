3362f54138b5a20b48cd58884fdfa477673f0ab0
Amir Shamsi <emirshamsi.uni@gmail.com>
2023-01-26 01:20:16 +0330

    patient file and doctor info were updated

--- back-end/appAuth/models.py
+++ back-end/appAuth/models.py
@@ -1,15 +1,19 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractUser
 from django.conf import settings
 import uuid
 from appApi.models import City, ProvinceState, InsuranceCompany
 
+
 class CustomUserManager(BaseUserManager):
-    def create_user(self, personal_ID, password, email,  **extra_fields):
+    def create_user(self, personal_ID, password, email, **extra_fields):
         user = self.model(personal_ID=personal_ID, email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save()
         return user
+
 
 class CustomUser(AbstractUser):
     username = None
@@ -25,6 +29,7 @@ class CustomUser(AbstractUser):
     # these fields are related to doctors
     is_doctor = models.BooleanField(default=False, blank=True, null=True)
     doctor_code = models.CharField(max_length=6, default=None, blank=True, null=True)
+    expertise = models.CharField(blank=True, null=True, default='', max_length=255)
 
     USERNAME_FIELD = 'personal_ID'
     REQUIRED_FIELDS = []
@@ -39,11 +44,15 @@ class CustomUser(AbstractUser):
 
 class PatientFile(models.Model):
     patient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="file")
+    disease = models.CharField(max_length=255)
     file_ID = models.CharField(unique=True, default=uuid.uuid4().hex[:6].upper(), max_length=6)
     detail = models.TextField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now=True)
+    appointment_date = models.DateField(null=True, blank=True, default=datetime.date.today())
     # this field determines every patient doctor(s)
-    patient_doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="doctor", default=None)
+    patient_doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="doctor",
+                                       default=None)
+
 
 class UserHashTable(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
